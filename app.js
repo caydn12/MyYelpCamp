4452ce434dcbfa9bd8f591495178fa880292f2d0
Caydn Lofton <caydnlofton@gmail.com>
2017-04-23 22:42:55 +0000

    Made a page to create a new campground

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 var campgrounds = [
         { Name: "Salmon Creek", Image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
@@ -7,6 +8,7 @@ var campgrounds = [
         { Name: "Mountain Goat's Rest", Image: "http://photosforclass.com/download/7121865553" }
     ];
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
@@ -14,7 +16,25 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-     res.render("campgrounds", {campgrounds: campgrounds})
+    res.render("campgrounds", {campgrounds: campgrounds})
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
+
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.campgroundName;
+    var image = req.body.campgroundImage;
+    
+    var newCampground = {
+        Name: name,
+        Image: image
+    };
+    
+    campgrounds.push(newCampground);
+    
+    res.redirect("campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
