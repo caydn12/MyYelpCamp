b0d9ad215c15a02d0a1bdbf5295cdfe1ea7b25cf
Caydn Lofton <caydnlofton@gmail.com>
2017-04-24 18:20:40 +0000

    Connected database to application through mongoose. Made sure the campgrounds page could load in data through the database and new campgrounds could be added to the database.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var campgrounds = [
-        { Name: "Salmon Creek", Image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-        { Name: "Granite Hill", Image: "http://photosforclass.com/download/2602356334" },
-        { Name: "Mountain Goat's Rest", Image: "http://photosforclass.com/download/7121865553" },
-        { Name: "Salmon Creek", Image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg" },
-        { Name: "Granite Hill", Image: "http://photosforclass.com/download/2602356334" },
-        { Name: "Mountain Goat's Rest", Image: "http://photosforclass.com/download/7121865553" },
-        { Name: "Salmon Creek", Image: "http://photosforclass.com/download/2602356334" },
-        { Name: "Granite Hill", Image: "http://photosforclass.com/download/2602356334" },
-        { Name: "Mountain Goat's Rest", Image: "http://photosforclass.com/download/7121865553" },
-    ];
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    campName: String,
+    campImage: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -22,7 +21,13 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds})
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds})
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -34,13 +39,17 @@ app.post("/campgrounds", function(req, res) {
     var image = req.body.campgroundImage;
     
     var newCampground = {
-        Name: name,
-        Image: image
+        campName: name,
+        campImage: image
     };
     
-    campgrounds.push(newCampground);
-    
-    res.redirect("campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
