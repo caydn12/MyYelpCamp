0ab3325736d8bdbbdb730cf6757c4fdd940348a8
Caydn Lofton <caydnlofton@gmail.com>
2017-05-01 19:37:02 +0000

    Added styling to the show pages and updated some seeding.

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Clouds Rest", 
         image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
-        description: "Blah Blah Blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nibh est. Morbi in sem justo. Donec eu nibh et augue laoreet bibendum ultricies vitae nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In malesuada cursus justo. Ut fermentum sapien et fringilla fringilla. Sed maximus erat elit, pellentesque dictum diam faucibus in. Fusce eget justo viverra, consequat ex et, porttitor est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras vulputate lobortis cursus. Morbi non mauris sit amet dolor vestibulum scelerisque. Quisque feugiat sem et lectus eleifend, sed condimentum augue sagittis. Curabitur sit amet orci ut orci ultrices ultricies ac a sem. Nulla pretium ut massa non interdum. Aliquam aliquet nec velit ut tempor. "
     },
     {
         name: "Desert Mesa", 
         image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg",
-        description: "Blah Blah Blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nibh est. Morbi in sem justo. Donec eu nibh et augue laoreet bibendum ultricies vitae nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In malesuada cursus justo. Ut fermentum sapien et fringilla fringilla. Sed maximus erat elit, pellentesque dictum diam faucibus in. Fusce eget justo viverra, consequat ex et, porttitor est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras vulputate lobortis cursus. Morbi non mauris sit amet dolor vestibulum scelerisque. Quisque feugiat sem et lectus eleifend, sed condimentum augue sagittis. Curabitur sit amet orci ut orci ultrices ultricies ac a sem. Nulla pretium ut massa non interdum. Aliquam aliquet nec velit ut tempor. "
     },
     {
         name: "Canyon Floor", 
-        image: "http://www.photosforclass.com/download/2182093741",
-        description: "Blah Blah Blah"
+        image: "https://i.cbc.ca/1.3928992.1484065378!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/tunnel-mountain-village-i-campground-banff.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nibh est. Morbi in sem justo. Donec eu nibh et augue laoreet bibendum ultricies vitae nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In malesuada cursus justo. Ut fermentum sapien et fringilla fringilla. Sed maximus erat elit, pellentesque dictum diam faucibus in. Fusce eget justo viverra, consequat ex et, porttitor est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras vulputate lobortis cursus. Morbi non mauris sit amet dolor vestibulum scelerisque. Quisque feugiat sem et lectus eleifend, sed condimentum augue sagittis. Curabitur sit amet orci ut orci ultrices ultricies ac a sem. Nulla pretium ut massa non interdum. Aliquam aliquet nec velit ut tempor. "
     },
 ]
 
